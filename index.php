aec0c627b154b3a0c353d44dd1f3700259612051
fricilialaila <fricilialaila@gmail.com.com>
2023-11-14 11:49:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apikasi Cafe Senja</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Cafe Senja</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></sssssssssssli>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+            </ul>
+        </nav>
+</header>
+
+<section class="menu">
+    <h2>Menu Cafe Senja</h2>
+    <div class="menu-item">
+        <img src="img/Bakso-Granat.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>
+            Deskrisi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : Rp. 10.000</p>
+        <button class="order-button">Pesan</button>
+    </div>
+    <div class="menu-item">
+        <img src="img/risol mayo.jpg" alt="menu 1">
+        <h3>Risol Mayo</h3>
+        <p>
+            Deskrisi tentang makanan misalnya nilai gizi dsb.
+        </p>
+        <p class="harga">Harga : Rp. 10.000</p>
+        <button class="order-button">Pesan</button>
+    </div>
+</section>
+
+    <section class="contact">
+        <h2>Hubungi kami</h2>
+        <p>Jika ada pertanyaan, saran atau kritik hubungi kami di:</p>
+        <p>Wa 0812344</p>
+        <p>Email : admin@gmail.com</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Cafe Senja .Hak Cipta di Lindungi</p>
+</footer>
+</body>
+</html>
